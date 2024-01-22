f888b6e79daa64a18ed7bc9b04ee97b9dba26941
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-01-22 15:21:05 +0500

    Fix error handling in POST /friends endpoint

--- index.js
+++ index.js
@@ -21,15 +21,14 @@ app.use(express.json());
 // ### POST REQUESTS ###
 app.post("/friends", (req, res) => {
   if (!req.body.name) {
-    res.status(400).json({
+    return res.status(400).json({
       error: "Missing friend name",
     });
   }
-
   const newFriend = { name: req.body.name, id: friends.length };
   friends.push(newFriend);
 
-  res.json(newFriend);
+  res.status(200).json(newFriend);
 });
 
 app.post("/messages", (req, res) => {
